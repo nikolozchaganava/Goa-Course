5277b7e10a5c96c1bc11667264a52edb6e7a6e08
Chago <137945551+nikolozchaganava@users.noreply.github.com>
2024-03-26 19:01:56 +0400

    Add files via upload

---
+++ HTML & CSS/Homeworks/Day 54/1/script.js
@@ -0,0 +1,53 @@
+// const
+const name_age_me_const= 'Nikoloz Chaganava 13'
+const name_age_father_const= 'Shavleg Chaganava 50'
+const name_age_mom_const= 'Elene Lukava 40'
+const name_age_sister_const= 'Mariam Chaganava 12'
+const name_age_grandma_const= 'Manana Dzidziguri 65'
+const name_age_grandpa_const= 'Murman Lukava 70'
+
+console.log(name_age_me_const)
+console.log(name_age_father_const)
+console.log(name_age_mom_const)
+console.log(name_age_sister_const)
+console.log(name_age_grandma_const)
+console.log(name_age_grandpa_const)
+
+//Let 
+let name_age_let= 'Nikoloz Chaganava 13'
+console.log(name_age_let)
+
+name_age_let= 'Shavleg Chaganava 50'
+console.log(name_age_let)
+
+name_age_let= 'Elene Lukava 40'
+console.log(name_age_let)
+
+name_age_let= 'Mariam Chaganava 12'
+console.log(name_age_let)
+
+name_age_let= 'Manana Dzidziguri 65'
+console.log(name_age_let)
+
+name_age_let= 'Murman Lukava 70'
+console.log(name_age_let)
+
+//In 20 years
+let age = 13
+console.log(age + 20)
+
+age = 50
+console.log(age + 20)
+
+age = 40
+console.log(age + 20)
+
+age = 12
+console.log(age + 20)
+
+age = 65
+console.log(age + 20)
+
+age = 70
+console.log(age + 20)
+
---
+++ HTML & CSS/Homeworks/Day 54/2/script.js
@@ -0,0 +1,8 @@
+// ტყეში არი 6ჩიტი თითო ჩიტს აქვს 2 ბუდე თითო ბუდეს აქვს 3კვერცი და 3 კვერცხიდან გამოიჩეკება 3 ბარტყი რამდენი ბარტყი გამოიჩეკება ჯამში 
+
+const bird = 6
+const nest = bird * 2
+const egg = nest * 3
+const nestling = egg
+
+console.log(egg)

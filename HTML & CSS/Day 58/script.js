b0aa46e8c44f20c6d3b61e4b8b00c26e479a8529
Chago <137945551+nikolozchaganava@users.noreply.github.com>
2024-04-16 19:50:45 +0400

    Add files via upload

---
+++ HTML & CSS/Day 58/script.js
@@ -0,0 +1,11 @@
+
+// let country = prompt();
+// let capital = prompt();
+
+// console.log(`Country: ${country}, Capital: ${capital}`);
+
+// let name = prompt("Whats your name?");
+
+// console.log(`Welcome, ${name}`);
+
+console.log('I \'ll')

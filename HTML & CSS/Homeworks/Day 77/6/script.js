8503af46d8abeb8bd1e30814b5710c7edb0c1b72
Chago <137945551+nikolozchaganava@users.noreply.github.com>
2024-06-11 21:09:42 +0400

    Add files via upload

---
+++ HTML & CSS/Homeworks/Day 77/6/script.js
@@ -0,0 +1,12 @@
+//Class work 6
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers[0] = 11;
+numbers[1] = 12;
+numbers[2] = 13;
+numbers[3] = 14;
+numbers[4] = 15;
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
